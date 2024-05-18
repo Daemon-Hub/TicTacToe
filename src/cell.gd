58ae0b6651bd411c9b8cfbd5dd5372085301d857
Daemon-Hub <islamkertov@gmail.com>
2024-05-18 13:53:54 +0300

    enable type hints

--- src/cell.gd
+++ src/cell.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 
-func _input_event(viewport, event, shape_idx):
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			$Polygon2D.color = Color.GREEN
+			
+			
